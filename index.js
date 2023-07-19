afaa1a68665c36a8d6dfd646d923378904dde857
Lucas <lucaas.meiraf@gmail.com>
2023-07-19 00:23:26 -0300

    Creating the validations for the regions that has the winClass that we also created here, and implementing some creative toughts like showing on the board the winning sequence and also the oposit of it

--- index.js
+++ index.js
@@ -14,9 +14,66 @@ function initialize(){
     updateTitle()
     gameBoard.forEach((element) =>{
         element.classList.remove('win')
+        element.classList.remove('lost')
         element.innerText = ''
+        element.classList.add('cursor-pointer')
         element.addEventListener('click', handleBoardClick)
     })
+}
+
+function getWinRegions() {
+    const winRegions = []
+    if(vBoard[0][0] && vBoard[0][0] === vBoard[0][1] && vBoard[0][0] === vBoard[0][2]) {
+        winRegions.push('0.0', '0.1', '0.2')
+    }
+    if(vBoard[1][0] && vBoard[1][0] === vBoard[1][1] && vBoard[1][0] === vBoard[1][2]) {
+        winRegions.push('1.0', '1.1', '1.2')
+    }
+    if(vBoard[2][0] && vBoard[2][0] === vBoard[2][1] && vBoard[2][0] === vBoard[2][2]) {
+        winRegions.push('2.0', '2.1', '2.2')
+    } 
+    if(vBoard[0][0] && vBoard[0][0] === vBoard[1][0] && vBoard[0][0] === vBoard[2][0]) {
+        winRegions.push('0.0', '1.0', '2.0')
+    }
+    if(vBoard[0][1] && vBoard[0][1] === vBoard[1][1] && vBoard[0][1] === vBoard[2][1]) {
+        winRegions.push('0.1', '1.1', '2.1')
+    }
+    if(vBoard[0][2] && vBoard[0][2] === vBoard[1][2] && vBoard[0][2] === vBoard[2][2]) {
+        winRegions.push('0.2', '1.2', '2.2')
+    } 
+    if(vBoard[0][0] && vBoard[0][0] === vBoard[1][1] && vBoard[0][0] === vBoard[2][2]) {
+        winRegions.push('0.0', '1.1', '2.2')
+    } 
+    if(vBoard[0][2] && vBoard[0][2] === vBoard[1][1] && vBoard[0][2] === vBoard[2][0]) {
+        winRegions.push('0.2', '1.1', '2.0')
+    }
+    return winRegions
+    
+}
+
+function handleWin(regions){
+    regions.forEach((region) =>{
+        //Para cada região dentro do retorno da função winregions, vamos add a class win
+        document.querySelector('[data-region="'+ region +'"]').classList.add('win')
+    })
+
+    // Tentativa de desablitar os campos em branco quando um jogador ganhar.
+    gameBoard.forEach((region) => {
+        if(!region.value){
+            disableRegion(region)
+        }
+    })
+
+    // Tentativa de marcar as regiões que não formaram uma sequência de vitória
+    gameBoard.forEach((region) =>{
+        if(!region.classList.contains('win') && region.innerText !== ''){
+            region.classList.add('lost')
+        }
+    })
+
+    const playerName = document.getElementById(turnplayer).value
+    document.querySelector('h2').innerHTML = playerName + ' venceu!'
+    
 }
 
 function handleBoardClick(ev){
@@ -36,10 +93,19 @@ function handleBoardClick(ev){
     console.clear()
     console.table(vBoard)
     disableRegion(span)
+    const winRegions = getWinRegions()
+    if(winRegions.length > 0){
+        handleWin(winRegions)
+    }else if(vBoard.flat().includes('')){
+        turnplayer = turnplayer === 'player1' ? 'player2' : 'player1'
+        updateTitle()
+    }else{
+        document.querySelector('h2').innerHTML = 'Empate!'
+    }
 }
 
 function disableRegion(element){
-    element.style.cursor = 'default'
+    element.classList.remove('cursor-pointer')
     element.removeEventListener('click', handleBoardClick)
 }
 
